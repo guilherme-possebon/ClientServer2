32b4accbe6d984759adb84739ffa5011c56cf3ff
Guilherme Possebon <gpossebon67@gmail.com>
2025-02-09 15:21:36 -0300

    Update da forma de fazer email

--- Aula2 - Email/email.ts
+++ Aula2 - Email/email.ts
@@ -1,32 +1,20 @@
-import * as nodemailer from "nodemailer";
-import * as fs from "fs";
+import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
 
-console.log("Email");
-
-let emailConfig = {
-  host: "smtp.mailersend.net",
-  port: 587,
-  auth: {
-    user: "MS_q2H7LQ@trial-v69oxl5n95dl785k.mlsender.net",
-    pass: "GDHsGziMsC9hEjkx",
-  },
-};
-
-let html = fs.readFileSync("email.html");
-
-let mailOptions = {
-  from: "teste@trial-v69oxl5n95dl785k.mlsender.net",
-  to: "gpossebon67@gmail.com",
-  subject: "Estou enviando um email pelo TS",
-  html: html,
-};
-
-let transporter = nodemailer.createTransport(emailConfig);
-
-transporter.sendMail(mailOptions, function (error, info) {
-  if (error) {
-    console.log("Erro ao enviar email: " + error);
-  } else {
-    console.log("Email enviado: " + info.response);
-  }
+console.log(process.env.API_KEY);
+const mailerSend = new MailerSend({
+  apiKey: process.env.API_KEY,
 });
+
+const sentFrom = new Sender("you@yourdomain.com", "Your name");
+
+const recipients = [new Recipient("your@client.com", "Your Client")];
+
+const emailParams = new EmailParams()
+  .setFrom(sentFrom)
+  .setTo(recipients)
+  .setReplyTo(sentFrom)
+  .setSubject("This is a Subject")
+  .setHtml("<strong>This is the HTML content</strong>")
+  .setText("This is the text content");
+
+await mailerSend.email.send(emailParams);
